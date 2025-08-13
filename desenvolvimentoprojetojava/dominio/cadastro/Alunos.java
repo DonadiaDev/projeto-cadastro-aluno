cd350033b507f49b52dc85be98fb3060ec279cfd
DonadiaDev <matheusdonaal@gmail.com>
2025-08-12 22:10:03 -0300

    criação do sistema de cadastro de alunos usando função Scanner

--- desenvolvimentoprojetojava/dominio/cadastro/Alunos.java
+++ desenvolvimentoprojetojava/dominio/cadastro/Alunos.java
@@ -1,6 +1,29 @@
 package desenvolvimentoprojetojava.dominio.cadastro;
 
+import desenvolvimentoprojetojava.dominio.alunos.CadastroAppAlunos;
+
 import java.util.Scanner;
 
 public class Alunos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Sistema de Cadastro de Alunos ===");
+
+        System.out.println("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a idade do aluno: ");
+        int idade = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite a matrícula do aluno: ");
+        String matricula = scanner.nextLine();
+
+        CadastroAppAlunos cadastroAppAlunos = new CadastroAppAlunos(nome, idade, matricula);
+
+        System.out.println("\ncadastroAppAlunos");
+        System.out.println(cadastroAppAlunos);
+
+        scanner.close();
+    }
 }
